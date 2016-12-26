a52233c076ca6b8ac0a004e9cd546976333ce7d8
Geoff McLane <ubuntu@geoffair.info>
2016-12-26 20:56:31 +0100

    If MSVC find dbg and rel SQLitet3 libs

--- CMakeModules/FindSQLite3.cmake
+++ CMakeModules/FindSQLite3.cmake
@@ -26,11 +26,50 @@ FIND_PATH(SQLITE3_INCLUDE_DIR sqlite3.h
     ${ADDITIONAL_LIBRARY_PATHS}
   )
 
-FIND_LIBRARY(SQLITE3_LIBRARY NAMES sqlite3 sqlite3
-    HINTS $ENV{SQLITE3DIR}
-    PATH_SUFFIXES lib64 lib libs64 libs libs/Win32 libs/Win64
-    PATHS ${ADDITIONAL_LIBRARY_PATHS}
-  )
+if (MSVC)
+    FIND_LIBRARY(SQLITE3_LIB_DBG NAMES sqlite3d
+        HINTS ${SQLITE3DIR} $ENV{SQLITE3DIR}
+        PATH_SUFFIXES lib lib64 libs64 libs libs/Win32 libs/Win64
+        PATHS ${ADDITIONAL_LIBRARY_PATHS}
+      )
+    FIND_LIBRARY(SQLITE3_LIB_REL NAMES sqlite3
+        HINTS ${SQLITE3DIR} $ENV{SQLITE3DIR}
+        PATH_SUFFIXES lib lib64 libs64 libs libs/Win32 libs/Win64
+        PATHS ${ADDITIONAL_LIBRARY_PATHS}
+      )
+    if (SQLITE3_LIB_DBG AND SQLITE3_LIB_REL)
+        set(SQLITE3_LIBRARY
+            optimized ${SQLITE3_LIB_REL}
+            debug ${SQLITE3_LIB_DBG})
+    else ()
+        if (SQLITE3_LIB_REL)
+            set(SQLITE3_LIBRARY ${SQLITE3_LIB_REL})
+        endif ()
+    endif ()
+else ()
+    FIND_LIBRARY(SQLITE3_LIBRARY NAMES sqlite3
+        HINTS $ENV{SQLITE3DIR}
+        PATH_SUFFIXES lib64 lib libs64 libs libs/Win32 libs/Win64
+        PATHS ${ADDITIONAL_LIBRARY_PATHS}
+      )
+endif ()
+
+if (SQLITE3_VERBOSE)
+    set(msg "FindSQLite3:")
+    if(SQLITE3_INCLUDE_DIR)
+        set(msg "${msg} found header sqlite3.h in ${SQLITE3_INCLUDE_DIR}")
+    else()
+        set(msg "${msg} header sqlite3.h not found")
+    endif()
+	if(SQLITE3_LIBRARY)
+		set(msg "${msg} found library ${SQLITE3_LIBRARY}")
+	else()
+		set(msg "${msg} libraries shp sqlites lib not found")
+	endif()
+	message(STATUS "${msg}")
+endif ()    
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(SQLITE3 DEFAULT_MSG SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR)
+
+# eof
